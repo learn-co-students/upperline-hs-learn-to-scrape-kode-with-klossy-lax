6b0604649efd731eb1b15411c8cde36ce03eeb6f
annebelleo <annebelleolminkhof@gmail.com>
2016-12-20 07:16:53 +0000

    not done lol

--- fidi_scraper.rb
+++ fidi_scraper.rb
@@ -1,1 +1,42 @@
-fidi_scraper.rb
+require 'nokogiri'
+require 'open-uri'
+
+fidi_html = open('https://s3-us-west-2.amazonaws.com/nokogiri-scrape/index.html')
+#puts fidi_html
+
+fidi_nokogiri = Nokogiri::HTML(fidi_html)
+#puts fidi_nokogiri
+
+#title = fidi_nokogiri.css(“h1”).text
+#puts title
+
+sophies = fidi_nokogiri.css("div.restaurant.r3 h2").text
+#puts sophies
+
+restaurants_names = fidi_nokogiri.css("div.restaurant h2").collect do |name|
+    name.text
+end
+#puts restaurants_names
+
+fidi_dining = fidi_nokogiri.css("div.header h1").text
+puts fidi_dining
+
+open_kitchen = fidi_nokogiri.css("div.restaurant.r5 h2").text
+puts open_kitchen
+
+grill_phone = fidi_nokogiri.css("div.restaurant.r1 h3").text
+puts grill_phone
+
+tgi_review = fidi_nokogiri.css("div.restaurant.r9 div.photo-desc h4").text
+puts tgi_review
+
+justinos_stars = fidi_nokogiri.css("div.restaurant.r6 p").text
+puts justinos_stars
+
+flavors_pic = fidi_nokogiri.css("div.restaurant.r7 div.photo-desc img.src").text
+puts flavors_pic
+
+restaurants_with_stars = fidi_nokogiri.css("div.restaurant h2", "div.restaurant p").each do |name, stars|
+    puts "#{name.text}"
+    end
+puts restaurants_with_stars
